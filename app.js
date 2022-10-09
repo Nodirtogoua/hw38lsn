db54a8ad272ceec9df92ec110cb99d38c172ecd9
nodirtogou <108736253+Nodirtogoua@users.noreply.github.com>
2022-10-09 23:14:44 +0400

    complete section

---
+++ app.js
@@ -0,0 +1,42 @@
+let input = document.querySelector(".input");
+// let del = document.querySelector(".delete");
+let numbers = document.querySelector(".num");
+let amallar = document.querySelector(".amallar");
+let equal = document.querySelector(".teng");
+let btns = document.querySelectorAll("button");
+
+input.innerHTML = 0;
+console.log(btns);
+
+for (i of btns) {
+    i.addEventListener("click", (event) => {
+        if (event.target.innerHTML === 'C') {
+            input.innerHTML = '0';
+        }
+        else if (event.target.innerHTML === 'Del') {
+            let val = input.innerHTML;
+            val = val.split("");
+            val.pop();
+            input.innerHTML = val.join("");
+            input.innerHTML == "" ? input.innerHTML = "0" : input.innerHTML
+            val = "";
+        }
+        else if (event.target.innerHTML === "=") {
+            console.log(input.innerHTML);
+            console.log(eval(input.innerHTML));
+            input.innerHTML = eval(input.innerHTML);
+        }
+        else {
+            input.innerHTML = input.innerHTML.split("")[0] == "0" ?
+                input.innerHTML.split("").slice(1, input.innerHTML.split("").length).join("") : input.innerHTML
+            input.textContent += event.target.innerHTML;
+        }
+    })
+}
+
+// for (index of amallar) {
+//     index.addEventListener("click", (e) => {
+//         input.innerHTML = input.innerHTML.split("")[e] == "+" ?
+//             input.innerHTML.split("").slice(1, input.innerHTML.split("").length).join("") : input.innerHTML
+//         input.textContent = e.target.innerHTML;
+//     })
